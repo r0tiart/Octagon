0a6b608447af52da351942ed4159f8239b9c011a
r0tiart <antley.chu@gmail.com>
2018-01-12 10:23:47 -0500

    started form and added a validator for firstname

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Contact = props => {
-  return (
-       <div className="main" > 
-        <h1>Contact Form</h1>
-        
-       </div>
-  );
-};
+
+class Contact extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+      email: '',
+      avatar: ''
+    };
+  }
+
+  validateFirstName = event => {
+    var letters = /^[A-Za-z]+$/;  
+
+    if( event.target.value.match(letters) ){
+      this.setState({
+        [event.target.className]: event.target.value        
+      });
+
+      return true;
+    }
+    else {
+      alert("characters only")
+      event.target.value = event.target.defaultValue
+      return false;
+    }
+  }
+
+render(){
+    return (
+       <form className="form">
+         <h2>Sign up</h2>
+         <div className="form-group">
+           <label htmlFor="firstName">First Name</label>
+           <input type="text" className="firstName" onChange={ this.validateFirstName.bind(this) }
+             name="firstName" />
+         </div>
+         <div className="form-group">
+           <label htmlFor="email">Email address</label>
+           <input type="email" className="form-control"
+             name="email" />
+         </div>
+         <button type="submit" className="btn btn-primary">
+            Sign up
+         </button>
+       </form>
+    );
+  }
+};  
+
 
 export default Contact;
