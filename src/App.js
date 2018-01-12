2d212e7b126a72f786c5f666d1f2e44e3f16050c
r0tiart <antley.chu@gmail.com>
2018-01-11 19:17:04 -0500

    moved home page to it's own component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 import logo from './logo.svg';
 import NavBar  from './components/NavBar';
 import './App.css';
+import HomePage from './components/Home';
 
 class App extends Component {
   render() {
@@ -14,30 +15,16 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Router>
-            <div className="App">
               <NavBar />
-              <Route exact path="/"  />
-              <Route path='/contact'  />
-            </div>
         </Router>
         </header>
 
-       <div className="main" > 
-         <div className="heroImage-container" id="fullwidth"> 
-          <img
-              src={'http://www.returnofkings.com/wp-content/uploads/2015/01/american-hero1.jpg'} className="heroImage" id="reduceHeight"
-          />
-         </div>
-
-         <div className="text" >
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-
-         </div>
-       </div>
+        <Router>
+          <div className="main">
+            <Route path="/" component={HomePage}/>
+            <Route path="/contact" render={() => <div>Contact</div>} />
+          </div>
+        </Router>
 
        <div className="footer" >
         <img src={logo} className="footer-logo" alt="logo" />
@@ -47,8 +34,6 @@ class App extends Component {
 
             <a href="/terms">Terms of Use</a>
           <p>© 2018 Octagon</p>
-
-
         </div>
        </div>
       </div>
